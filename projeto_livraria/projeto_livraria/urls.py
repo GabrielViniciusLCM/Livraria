7528b23bc0eec4c4abf4cadeb1a39a2dbef79aea
Gabriel Vinicius <gabrielviniciuslcm@gmail.com>
2024-09-23 20:32:00 -0300

    feat: projeto configurado

--- projeto_livraria/projeto_livraria/urls.py
+++ projeto_livraria/projeto_livraria/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
+from app_livraria import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # rota/view responsável/nome de referência
     # livraria.com
-    path(''),
+    path('', views.home, name ='home'),
     # livraria.com/teste
-    path('teste/')
+    # path('teste/')
 ]
